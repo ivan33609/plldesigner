25c9611f94b9d3bb51f91091f3398f04d9cb4258
Juan Felipe Osorio <jfosorio@gmail.com>
2014-12-07 12:31:12 +0100

    Split the test in different files, similarly to sympy

--- plldesigner/sdmod.py
+++ plldesigner/sdmod.py
@@ -178,12 +178,10 @@ def gen_mash(order, n, k, init=()):
             if state0[j] > _maxvalue:
                 overflow0[j] = 1
                 state0[j] -= _maxvalue + 1
-
             state1[j] = state1[j - 1] + state0[j]
             if state1[j] > _maxvalue:
                 overflow1[j] = 1
                 state1[j] -= _maxvalue + 1
-
             state2[j] = state2[j - 1] + state1[j]
             if state2[j] > _maxvalue:
                 overflow2[j] = 1
@@ -237,59 +235,3 @@ def L_mash_dB(m, fref, n=1.0):
         (2 * sin(pi * fm / fref)) ** (2 * (m - 1)) / n ** 2)
     ldbc = Pnoise.with_function(func_ldbc, fc=fref, label='sdm')
     return ldbc
-
-
-"""
-Testing
-"""
-import unittest
-
-class Test_sdmod(unittest.TestCase):
-    def test_mean_value(self):
-
-        from numpy.testing import assert_almost_equal
-        import numpy.random as rnd
-
-        # Test the floating number is well reproduced
-        # for the mash, order 1, the signal is fix because other wise
-        # it can fail quite often
-        floatnum = 0.309 * np.ones(100000)
-        # order one
-        sequence, period = gen_mash(1, 19, (floatnum * 2 ** 19).astype(int))
-        assert_almost_equal(sequence.mean(), floatnum.mean(), 4)
-        assert period == None
-        sequence, period = gen_mash(1, 19, 0.25 * np.ones(1000) * 2 ** 19)
-        assert period == 4
-        floatnum = rnd.rand() * np.ones(100000)
-        # order two
-        sequence, period = gen_mash(2, 19, (floatnum * 2 ** 19).astype(int))
-        assert_almost_equal(sequence.mean(), floatnum.mean(), 4)
-        assert period == None
-        sequence, period = gen_mash(2, 19, 0.25 * np.ones(1000) * 2 ** 19)
-        assert period == 8
-
-        # order three
-        sequence, period = gen_mash(3, 19, (floatnum * 2 ** 19).astype(int))
-        assert_almost_equal(sequence.mean(), floatnum.mean(), 4)
-        assert period == None
-        sequence, period = gen_mash(3, 19, 0.25 * np.ones(1000) * 2 ** 19)
-        assert_almost_equal(sequence.mean(), 0.25, 2)
-        assert period == 8
-        sequence, period = gen_mash(3, 19, 0.25 * np.ones(1000) * 2 ** 19, init=(1,0,0))
-        assert_almost_equal(sequence.mean(), 0.25, 2)
-        assert period == None
-
-        # order four
-        sequence, period = gen_mash(4, 19, (floatnum * 2 ** 19).astype(int))
-        assert_almost_equal(sequence.mean(), floatnum.mean(), 4)
-        assert period == None
-        sequence, period = gen_mash(4, 19, 0.25 * np.ones(1000) * 2 ** 19)
-        assert period == 16
-
-        # Using the class the test is done as:
-        sd_mash = SDModulator('mash', 3, 19,
-                              (np.array([0.323232] * 100000) * 2 ** 19).astype(int))
-        assert_almost_equal(sd_mash.seq.mean(), 0.323232, 4)
-
-if __name__ == "__main__":
-    unittest.main()
